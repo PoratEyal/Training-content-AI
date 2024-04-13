b7335ae920ecd1f23167516ccc9acd0d70830e44
eyal porat <eyal1.porat@gmail.com>
2024-04-13 20:43:43 +0300

    added whatsup and docs import files

--- src/pages/activity/activity.js
+++ src/pages/activity/activity.js
@@ -22,15 +22,23 @@ function Activity() {
 
     const [iconClickedPoint, setIconClickedPoint] = useState(false)
     const [moreDataPoint, setMoreDataPoint] = useState(false)
+    const [whatsupPoint, setWhatsupPoint] = useState(false)
+    const [docsPoint, setDocsPoint] = useState(false)
 
     const [iconClickedContent, setIconClickedContent] = useState(false)
     const [moreDataContent, setMoreDataContent] = useState(false)
+    const [whatsupContent, setWhatsupContent] = useState(false)
+    const [docsContent, setDocsContent] = useState(false)
 
     const [iconClickedScoutingTime, setIconClickedScoutingTime] = useState(false)
     const [moreDataScoutingTime, setMoreDataScoutingTime] = useState(false)
+    const [whatsupScoutingTime, setWhatsupScoutingTime] = useState(false)
+    const [docsScoutingTime, setDocsScoutingTime] = useState(false)
 
     const [iconClickedPlaying, setIconClickedPlaying] = useState(false)
     const [moreDataPlaying, setMoreDataPlaying] = useState(false)
+    const [whatsupPlaying, setWhatsupPlaying] = useState(false)
+    const [docsPlaying, setDocsPlaying] = useState(false)
 
     const generateAgain = async (index) => {
         if(index == 1){
@@ -86,6 +94,184 @@ function Activity() {
         }
     }
 
+    const importWhatsup = (index) => {
+        if (index === 1) {
+            setWhatsupPoint(true)
+            const message = encodeURIComponent(data.pointOfView.data);
+            const whatsappUrl = `https://wa.me/?text=${message}`;
+    
+            window.open(whatsappUrl, '_blank');
+            setWhatsupPoint(false)
+        }
+        if (index === 2) {
+            setWhatsupContent(true)
+            const message = encodeURIComponent(data.contentActivity.data);
+            const whatsappUrl = `https://wa.me/?text=${message}`;
+    
+            window.open(whatsappUrl, '_blank');
+            setWhatsupContent(false)
+        }
+        if (index === 3) {
+            setWhatsupScoutingTime(true)
+            const message = encodeURIComponent(data.scoutingTime.data);
+            const whatsappUrl = `https://wa.me/?text=${message}`;
+    
+            window.open(whatsappUrl, '_blank');
+            setWhatsupScoutingTime(false)
+        }
+        if (index === 4) {
+            setWhatsupPlaying(true)
+            const message = encodeURIComponent(data.playingTime.data);
+            const whatsappUrl = `https://wa.me/?text=${message}`;
+    
+            window.open(whatsappUrl, '_blank');
+            setWhatsupPlaying(false)
+        }
+    }
+    
+    const importDocs = async (index) => {
+        if (index === 1) {
+            setDocsPoint(true);
+            console.log(data.pointOfView.data);
+    
+            const newWindow = window.open('', '_blank');
+            if(newWindow) {
+                newWindow.document.write(`
+                    <html>
+                    <head>
+                        <title>נקודת מבט</title>
+                        <style>
+                            body { 
+                                text-align: right; 
+                                direction: rtl; 
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                            h1 {
+                                font-weight: bold;
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>נקודת מבט</h1>
+                        <pre>${data.pointOfView.data}</pre>
+                    </body>
+                    </html>
+                `);
+                newWindow.document.close();
+            } else {
+                alert('Pop-up blocker might have prevented opening the new window.');
+            }
+    
+            setDocsPoint(false);
+        }
+        if (index === 2) {
+            setDocsPoint(true);
+            console.log(data.pointOfView.data);
+    
+            const newWindow = window.open('', '_blank');
+            if(newWindow) {
+                newWindow.document.write(`
+                    <html>
+                    <head>
+                        <title>פעילות תוכן</title>
+                        <style>
+                            body { 
+                                text-align: right; 
+                                direction: rtl; 
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                            h1 {
+                                font-weight: bold;
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>פעילות תוכן</h1>
+                        <pre>${data.contentActivity.data}</pre>
+                    </body>
+                    </html>
+                `);
+                newWindow.document.close();
+            } else {
+                alert('Pop-up blocker might have prevented opening the new window.');
+            }
+    
+            setDocsPoint(false);
+        }
+        if (index === 3) {
+            setDocsPoint(true);
+            console.log(data.pointOfView.data);
+    
+            const newWindow = window.open('', '_blank');
+            if(newWindow) {
+                newWindow.document.write(`
+                    <html>
+                    <head>
+                        <title>זמן צופיות</title>
+                        <style>
+                            body { 
+                                text-align: right; 
+                                direction: rtl; 
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                            h1 {
+                                font-weight: bold;
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>זמן צופיות</h1>
+                        <pre>${data.scoutingTime.data}</pre>
+                    </body>
+                    </html>
+                `);
+                newWindow.document.close();
+            } else {
+                alert('Pop-up blocker might have prevented opening the new window.');
+            }
+    
+            setDocsPoint(false);
+        }
+        if (index === 4) {
+            setDocsPoint(true);
+            console.log(data.pointOfView.data);
+    
+            const newWindow = window.open('', '_blank');
+            if(newWindow) {
+                newWindow.document.write(`
+                    <html>
+                    <head>
+                        <title>זמן משחק</title>
+                        <style>
+                            body { 
+                                text-align: right; 
+                                direction: rtl; 
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                            h1 {
+                                font-weight: bold;
+                                font-family: 'Fredoka', sans-serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>זמן משחק</h1>
+                        <pre>${data.playingTime.data}</pre>
+                    </body>
+                    </html>
+                `);
+                newWindow.document.close();
+            } else {
+                alert('Pop-up blocker might have prevented opening the new window.');
+            }
+    
+            setDocsPoint(false);
+        }
+    }
+
     // Function to format the activity text with line breaks
     const formatTextWithLineBreaks = (text) => {
         return text.split('\n').map((line, index, array) => (
@@ -110,6 +296,10 @@ function Activity() {
                 <div className={styles.activity_div}>
                     <div className={styles.h2_icon_div}>
                         <h2>נקודת מבט</h2>
+                        <div className={styles.icons}>
+                            {!whatsupPoint ? <FaWhatsapp onClick={() => importWhatsup(1)}></FaWhatsapp> : <AiOutlineLoading></AiOutlineLoading>}
+                            {!docsPoint ? <BsFiletypeDocx onClick={() => importDocs(1)}></BsFiletypeDocx> : <AiOutlineLoading></AiOutlineLoading>}
+                        </div>
                     </div>
                     {formatTextWithLineBreaks(data.pointOfView.data)}
                     
@@ -133,19 +323,23 @@ function Activity() {
                 <div className={styles.activity_div}>
                     <div className={styles.h2_icon_div}>
                         <h2>פעילות תוכן</h2>
+                        <div className={styles.icons}>
+                            {!whatsupContent ? <FaWhatsapp onClick={() => importWhatsup(2)}></FaWhatsapp> : <AiOutlineLoading></AiOutlineLoading>}
+                            {!docsContent ? <BsFiletypeDocx onClick={() => importDocs(2)}></BsFiletypeDocx> : <AiOutlineLoading></AiOutlineLoading>}
+                        </div>
                     </div>
                     {formatTextWithLineBreaks(data.contentActivity.data)}
 
                     <div className={styles.buttons_div}>
                         <button onClick={() => generateAgain(2)} className={styles.button}>
                             <label onClick={() => generateAgain(2)}>דוגמא נוספת</label>
-                            {iconClickedPoint &&
+                            {iconClickedContent &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
 
                         <button onClick={() => moreData(2)} className={styles.button}>
                             <label onClick={() => moreData(2)}>הרחבת התוכן</label>
-                            {moreDataPoint &&
+                            {moreDataContent &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
                     </div>
@@ -155,19 +349,23 @@ function Activity() {
                 <div className={styles.activity_div}>
                     <div className={styles.h2_icon_div}>
                         <h2>זמן צופיות</h2>
+                        <div className={styles.icons}>
+                            {!whatsupScoutingTime ? <FaWhatsapp onClick={() => importWhatsup(3)}></FaWhatsapp> : <AiOutlineLoading></AiOutlineLoading>}
+                            {!docsScoutingTime ? <BsFiletypeDocx onClick={() => importDocs(3)}></BsFiletypeDocx> : <AiOutlineLoading></AiOutlineLoading>}
+                        </div>
                     </div>
                     {formatTextWithLineBreaks(data.scoutingTime.data)}
 
                     <div className={styles.buttons_div}>
                         <button onClick={() => generateAgain(3)} className={styles.button}>
                             <label onClick={() => generateAgain(3)}>דוגמא נוספת</label>
-                            {iconClickedPoint &&
+                            {iconClickedScoutingTime &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
 
                         <button onClick={() => moreData(3)} className={styles.button}>
                             <label onClick={() => moreData(3)}>הרחבת התוכן</label>
-                            {moreDataPoint &&
+                            {moreDataScoutingTime &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
                     </div>
@@ -177,25 +375,27 @@ function Activity() {
                 <div className={styles.activity_div}>
                     <div className={styles.h2_icon_div}>
                         <h2>זמן משחק</h2>
+                        <div className={styles.icons}>
+                            {!whatsupPlaying ? <FaWhatsapp onClick={() => importWhatsup(4)}></FaWhatsapp> : <AiOutlineLoading></AiOutlineLoading>}
+                            {!docsPlaying ? <BsFiletypeDocx onClick={() => importDocs(4)}></BsFiletypeDocx> : <AiOutlineLoading></AiOutlineLoading>}
+                        </div>
                     </div>
                     {formatTextWithLineBreaks(data.playingTime.data)}
 
                     <div className={styles.buttons_div}>
                         <button onClick={() => generateAgain(4)} className={styles.button}>
                             <label onClick={() => generateAgain(4)}>דוגמא נוספת</label>
-                            {iconClickedPoint &&
+                            {iconClickedPlaying &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
 
                         <button onClick={() => moreData(4)} className={styles.button}>
                             <label onClick={() => moreData(4)}>הרחבת התוכן</label>
-                            {moreDataPoint &&
+                            {moreDataPlaying &&
                                 <AiOutlineLoading className={styles.icon_more}></AiOutlineLoading>}
                         </button>
                     </div>
                 </div>}
-
-            {/* <a href={data.img[0]?.url} download={`image.jpg`}><img className={styles.img} src={data.img[0]?.url} alt="Generated img" /></a> */}
 
         </div>
     );
