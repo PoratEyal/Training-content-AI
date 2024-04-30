10f80f151d5ff9438a8abdfa037c4e2f78f767ad
eyal porat <eyal1.porat@gmail.com>
2024-04-30 22:39:42 +0300

    added SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { ContentProvider } from "./context/ContentContext";
 import Details from "./pages/Details/Details";
@@ -8,7 +10,6 @@ import PrivateRoutes from "./components/PrivateRoutes";
 import { ErrorContextProvider } from "./context/ErrorContext";
 import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
-// import Maintenance from "./pages/Maintenance/maintenance";
 
 function App() {
     return (
@@ -16,15 +17,41 @@ function App() {
             <ContentProvider>
                 <ReactNotifications />
                 <Router>
+                    <Helmet>
+                        <title>בונה פעולות לתנועות נוער</title>
+                        <meta name="description" content="צור פעולות מרתקות ומותאמות אישית עבור חניכיך תוך דקות ספורות באמצעות כלי בינה מלאכותית חדשניים. חסוך זמן, התמקד ביצירתיות, והענק לחניכים חוויה חינוכית בלתי נשכחת" />
+                    </Helmet>
                     <Routes>
                         <Route element={<PrivateRoutes />}>
-                            <Route path="/" element={<Details />} />
-                            <Route path="/choosePath" element={<ChoosePath />} />
-                            <Route path="/activity" element={<Activity />} />
+                            <Route path="/" element={
+                                <>
+                                    <Helmet>
+                                        <title>מילוי פרטים על הקבוצה</title>
+                                        <meta name="description" content="צור פעולות מרתקות ומותאמות אישית עבור חניכיך תוך דקות ספורות באמצעות כלי בינה מלאכותית חדשניים. חסוך זמן, התמקד ביצירתיות, והענק לחניכים חוויה חינוכית בלתי נשכחת" />
+                                    </Helmet>
+                                    <Details />
+                                </>
+                            } />
+                            <Route path="/choosePath" element={
+                                <>
+                                    <Helmet>
+                                        <title>בחרו את הפעולה המתאימה ביותר בשבילכם</title>
+                                        <meta name="description" content="צור פעולות מרתקות ומותאמות אישית עבור חניכיך תוך דקות ספורות באמצעות כלי בינה מלאכותית חדשניים. חסוך זמן, התמקד ביצירתיות, והענק לחניכים חוויה חינוכית בלתי נשכחת" />
+                                    </Helmet>
+                                    <ChoosePath />
+                                </>
+                            } />
+                            <Route path="/activity" element={
+                                <>
+                                    <Helmet>
+                                        <title>קבלת פעולה מותאמת אישית בשילוב בינה מלאכותית להדרכת תנועות נוער</title>
+                                        <meta name="description" content="צור פעולות מרתקות ומותאמות אישית עבור חניכיך תוך דקות ספורות באמצעות כלי בינה מלאכותית חדשניים. חסוך זמן, התמקד ביצירתיות, והענק לחניכים חוויה חינוכית בלתי נשכחת" />
+                                    </Helmet>
+                                    <Activity />
+                                </>
+                            } />
                         </Route>
-
                         <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-                        {/* <Route path="/" element={<Maintenance />} /> */}
                         <Route path={"*"} element={<Navigate replace to="/" />} />
                     </Routes>
                 </Router>
