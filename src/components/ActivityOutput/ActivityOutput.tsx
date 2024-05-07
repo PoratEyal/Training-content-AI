2be84ed88d7f0a922233806ae3fc1b042da6ccea
royporat <roy.porat@bloxroute.com>
2024-05-08 00:23:03 +0300

    fixes

--- src/components/ActivityOutput/ActivityOutput.tsx
+++ src/components/ActivityOutput/ActivityOutput.tsx
@@ -4,7 +4,6 @@ import { Activity, PathType } from "../../models/types/activity";
 import ReactMarkdown from "react-markdown";
 import MoreActions from "../MoreActions/MoreActions";
 import "./Markdown.css";
-import ShareBtns from "../ShareBtns/ShareBtns";
 
 type ActivityOutputProps = {
     pathActivity: PathType;
@@ -17,10 +16,7 @@ function ActivityOutput({ pathActivity }: ActivityOutputProps) {
 
     return (
         <section className={styles.activity_contianer} id="markdown">
-            <div className={styles.activity_top}>
                 <div className={styles.activity_title}>{name}</div>
-                <ShareBtns text={activity} />
-            </div>
 
             <ReactMarkdown>{activity}</ReactMarkdown>
 
