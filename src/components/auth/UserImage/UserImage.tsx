d9f5d378add9ac5e2b3d677dbfc52c92814b7192
eyal porat <eyal1.porat@gmail.com>
2024-05-11 12:29:54 +0300

    css page 3, root colors

--- src/components/auth/UserImage/UserImage.tsx
+++ src/components/auth/UserImage/UserImage.tsx
@@ -10,8 +10,8 @@ function UserImage({ img, size = "small" }: UserImageProps) {
     const fontSize = size === "small" ? "16px" : size === "medium" ? "18px" : "24px";
 
     return (
-        <div className={styles.user_profile_img} style={{ fontSize }}>
-            <img height={30} width={30} src={img} alt="Profile image" />
+        <div className={styles.img_div}>
+            <img className={styles.user_profile_img} src={img} alt="Profile image" />
         </div>
     );
 }
