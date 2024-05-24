21bd57dd9faccd7975393d8effe9caacde29a40e
royporat <roy.porat@bloxroute.com>
2024-05-24 19:13:51 +0300

    loading

---
+++ src/components/Loading/LoadingLogin/LoadingLogin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./LoadingLogin.module.css";
+
+function LoadingLogin() {
+    return <section className={styles.loading_login_container}>
+        טעינה
+    </section>;
+}
+
+export default LoadingLogin;
