89712bfb7fdd582a220d9c4b84582aa915e23f8e
royporat <roy.porat@bloxroute.com>
2024-05-25 18:51:20 +0300

    profile

--- src/components/auth/Profile/Profile.tsx
+++ src/components/auth/Profile/Profile.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UserImage from "../UserImage/UserImage";
 import styles from "./Profile.module.css";
 import Dropdown from "../../Layout/Dropdown/Dropdown";
 
 type ProfileProps = {
     img: string;
-    name: string;
 };
 
-function Profile({ img, name }: ProfileProps) {
+function Profile({ img }: ProfileProps) {
     const [isOpened, setIsOpened] = useState(false);
-    const handleOpen = () => setIsOpened(prev => !prev);
+    const handleOpen = () => setIsOpened((prev) => !prev);
     const handleClose = () => setIsOpened(false);
+    const [profileImg, setProfileImg] = useState<string>(img);
+
+    useEffect(() => {
+        if (img) setProfileImg(img);
+    }, [img]);
 
     return (
-        <div style={{position: "relative"}}>
+        <div style={{ position: "relative" }}>
             <div className={styles.user_profile} onClick={handleOpen}>
-                <UserImage img={img} size="small" />
+                <UserImage img={profileImg} size="small" />
                 {/* <div className={styles.user_profile_description}>
                     <span className={styles.user_profile_name}>{name}</span>
                 </div> */}
