3780d0bbe18ac8cc97aa32732bac392f14edaf86
eyal porat <eyal1.porat@gmail.com>
2024-05-25 19:40:42 +0300

    ui changes

--- src/components/PrivateRoutes.tsx
+++ src/components/PrivateRoutes.tsx
@@ -1,48 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import LimitRequest from "./popups/LimitRequests/LimitRequests";
-import TSCs from "./popups/TSCs/TSCs";
-import { NOT_REGISTER_LIMIT } from "../models/constants";
-import { useAuthContext } from "../context/AuthContext";
-import { forLongTime } from "../utils/time";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const PrivateRoutes = () => {
-    const location = useLocation();
-    const { cookies, setCookie, unRegisterLimit } = useAuthContext();
-    const { isLoggedIn, loading } = useAuthContext();
-    const [block, setBlock] = useState(false);
-    const [tscs, setTscs] = useState(false);
-
-    useEffect(() => {
-        if(location.pathname === "/") return;
-        if (loading || isLoggedIn) return;
-        if (unRegisterLimit >= NOT_REGISTER_LIMIT + 1) {
-            setBlock(true);
-        }
-    }, [unRegisterLimit]);
-
-    useEffect(() => {
-        if (cookies["user-consent"] === undefined) {
-            setTscs(true);
-        }
-    }, [cookies]);
-
-    const handleAcceptTerms = () => {
-        setCookie("user-consent", "accepted", {
-            path: "/",
-            expires: forLongTime,
-        });
-        setTscs(false);
-    };
-
-    const handleAcceptLimit = () => {
-        setBlock(false);
-    };
-
     return (
         <React.Fragment>
-            {block ? <LimitRequest handleAccept={handleAcceptLimit} /> : null}
-            {tscs ? <TSCs handleAccept={handleAcceptTerms} /> : null}
             <Outlet />
         </React.Fragment>
     );
