ec4c6c249f3501eb13fd07ef8da75bd2c14899cb
eyal porat <eyal1.porat@gmail.com>
2024-06-03 10:41:02 +0300

    ui changes

--- src/components/titles/ActivityReady/ActivityReady.tsx
+++ src/components/titles/ActivityReady/ActivityReady.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ActivityReady.module.css";
 
 function ActivityReady() {
-    return <h2 className={styles.activity_title}>הפעילות מוכנה</h2>;
+    return <h2 className={styles.activity_title}>הפעילות מוכנה 🥳</h2>;
 }
 
 export default ActivityReady;
