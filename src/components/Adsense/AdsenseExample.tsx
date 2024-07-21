f36d238136dcc164f1658e1c8fbc56b1d71a8b37
royporat <roy.porat@bloxroute.com>
2024-07-22 00:22:57 +0300

    init ads

---
+++ src/components/Adsense/AdsenseExample.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Adsense } from "@ctrl/react-adsense";
+import "./css/AdsenseStylesheet.css";
+
+function AdsenseExample() {
+    //console.log("Ads Shown")
+    return (
+        <Adsense
+            className="ExampleAdSlot"
+            client="ca-pub-1234"
+            slot="2222222"
+            adTest="on" //Dev Only
+        />
+    );
+}
+
+export default AdsenseExample;
