f7a6fe6dd3f8ba3ad82408298a3c1a492631886c
royporat <roy.porat@bloxroute.com>
2024-07-26 14:06:24 +0300

    ad

--- src/components/Adsense/AdsenseExample.tsx
+++ src/components/Adsense/AdsenseExample.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Adsense } from "@ctrl/react-adsense";
 
 function AdsenseExample() {
-    //console.log("Ads Shown")
     return (
         <Adsense
             className="ExampleAdSlot"
-            client="ca-pub-1234"
-            slot="2222222"
-            adTest="on" //Dev Only
-            style={{ width: 500, height: 100, backgroundColor: "black" }}
+            client="ca-pub-9858822058074702"
+            slot="9576195701"
+            // adTest="on" //Dev Only
+            // style={{ width: 300, height: 320, backgroundColor: "black" }}
+            style={{ width: 350, height: 100, backgroundColor: "black" }}
             layout="in-article"
-            format="fluid"
+            format="repv"
         />
     );
 }
