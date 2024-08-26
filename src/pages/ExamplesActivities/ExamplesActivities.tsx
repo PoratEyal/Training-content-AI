5bbf1cf39ffd010da4a5b71bb113070ccb56c9bb
PoratEyal <eyal1.porat@gmail.com>
2024-08-26 14:08:05 +0300

    ativity examples

--- src/pages/ExamplesActivities/ExamplesActivities.tsx
+++ src/pages/ExamplesActivities/ExamplesActivities.tsx
@@ -5,25 +5,39 @@ import PageLayout from "../../components/Layout/PageLayout/PageLayout";
 import route from "../../router/route.json";
 import ReactMarkdown from "react-markdown";
 import { Activities } from "../../models/resources/activities";
+import { useState } from "react";
 
 function ExamplesActivities() {
     const navigate = useNavigate();
+    const [selectedIndex, setSelectedIndex] = useState(2); // Initial state is 2, third activity
 
     const goBack = () => {
         navigate(-1);
     };
 
+    const handleSelectChange = (event) => {
+        setSelectedIndex(Number(event.target.value));
+    };
+
     return (
         <PageLayout path={route.examplesActivities} hasHeader={{ goBack, isBlur: true }} hasFade>
             <article className={styles.privacy_article}>
+                <h1 className={styles.page_title}>פעולות נפוצות</h1>
+                <section id="dropdown" className={styles.input_div}>
+                    <select className={styles.select} onChange={handleSelectChange} value={selectedIndex}>
+                        {Activities.map((activity, index) => (
+                            <option key={index} value={index}>
+                                {activity.title}
+                            </option>
+                        ))}
+                    </select>
+                </section>
                 <section id="markdown">
-                    <h1 className={styles.page_title}>פעולות נפוצות</h1>
-                    {Activities.map((activity, index) => (
-                        <div key={index}>
-                            <ReactMarkdown>{activity}</ReactMarkdown>
-                            <br />
+                    {selectedIndex !== null && (
+                        <div>
+                            <ReactMarkdown>{Activities[selectedIndex].content}</ReactMarkdown>
                         </div>
-                    ))}
+                    )}
                 </section>
             </article>
         </PageLayout>
