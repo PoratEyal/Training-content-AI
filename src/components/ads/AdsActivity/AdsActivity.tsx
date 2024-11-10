235c5b5b25e7d141230eb7572e92a2eb77afd9da
royporat <roy.porat@bloxroute.com>
2024-11-10 21:09:18 +0200

    ads

---
+++ src/components/ads/AdsActivity/AdsActivity.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+{
+    /* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9858822058074702"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-9858822058074702"
+     data-ad-slot="5162325824"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script> */
+}
+
+const AdsActivity = () => {
+    return (
+        <div>
+            <h1>Ads Activity</h1>
+        </div>
+    );
+};
+
+export default AdsActivity;
