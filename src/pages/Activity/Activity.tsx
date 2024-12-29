8bdc54e3d6f2d2c1d33b48a013878024d449653f
PoratEyal <eyal1.porat@gmail.com>
2024-12-29 22:49:34 +0200

    eyal - working on the more buttons in the activity :)

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -9,7 +9,8 @@ import ActivityReady from "../../components/titles/ActivityReady/ActivityReady";
 import helmet from "../../models/resources/helmet.json";
 import { ACTIVITY_AD_SLOT } from "../../models/constants/adsSlot";
 import { useAuthContext } from "../../context/AuthContext";
-import SaveBtn from "../../components/SaveBtn/SaveBtn";
+import SaveBtn from "../../components/ActivityMoreOptions/SaveBtn/SaveBtn";
+import MoreBtn from "../../components/ActivityMoreOptions/MoreBtn/MoreBtn";
 
 function Activity() {
   const { data, mainActivity } = useContentContext();
@@ -51,7 +52,7 @@ function Activity() {
         <section className={styles.activity_data_container}>
           <article>
             <ActivityOutput activity={mainActivity.activity} activityRef={activityRef} />
-            {isLoggedIn ? <SaveBtn activity={mainActivity} /> : null}
+            {isLoggedIn ? <MoreBtn activity={mainActivity} /> : null}
           </article>
           <div className={styles.padding} />
         </section>
