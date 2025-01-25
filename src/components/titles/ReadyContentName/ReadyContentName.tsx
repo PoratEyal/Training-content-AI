38542c95a4df60bdbaacfad2a61b4505a0b28cae
PoratEyal <eyal1.porat@gmail.com>
2025-01-25 22:34:16 +0200

    eyal p: little fixes

--- src/components/titles/ReadyContentName/ReadyContentName.tsx
+++ src/components/titles/ReadyContentName/ReadyContentName.tsx
@@ -3,13 +3,20 @@ import styles from "../ReadyContentName/ReadyContentName.module.css";
 type ActivityReadyProps = {
     subject: string;
     isMany?: boolean;
+    isLoading?: boolean;
 };
 
-function ReadyContentName({ subject, isMany = false }: ActivityReadyProps) {
+function ReadyContentName({ subject, isMany = false, isLoading = false }: ActivityReadyProps) {
     return (
         <h1 className={styles.activity_title}>
-            {isMany ? <span>פעולות בנושא</span> : <span>פעולה בנושא</span>}
-            <div>{subject}</div>
+            {isLoading ? (
+                <span>פעולות בנושא בטעינה...</span>
+            ) : (
+                <>
+                    {isMany ? <span>פעולות בנושא</span> : <span>פעולה בנושא</span>}
+                    <div>{subject}</div>
+                </>
+            )}
         </h1>
     );
 }
