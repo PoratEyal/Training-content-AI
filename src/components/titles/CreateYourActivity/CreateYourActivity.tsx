556239c82bd8a41cfc569436b077638bc3445351
PoratEyal <eyal1.porat@gmail.com>
2025-03-01 12:20:21 +0200

    more changes in the languege

--- src/components/titles/CreateYourActivity/CreateYourActivity.tsx
+++ src/components/titles/CreateYourActivity/CreateYourActivity.tsx
@@ -1,21 +1,32 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
 import styles from "./CreateYourActivity.module.css";
 
 function CreateYourActivity() {
-    return (
-        <div className={styles.create_your_activity_title}>
-            <h1>
-                צרו את<br></br> הפעולה שלכם
-            </h1>
-            <img
-                title="Sparks effect"
-                alt="Sparks effect"
-                src={"page3_effect.svg"}
-                loading="lazy"
-                width={23}
-                height={24}
-            ></img>
-        </div>
-    );
+  const { t, i18n } = useTranslation(); // uses the default namespace "translation"
+  const isEnglish = i18n.language === "en";
+
+  return (
+    <div
+      className={
+        isEnglish
+          ? `${styles.create_your_activity_title} ${styles.ltr}`
+          : styles.create_your_activity_title
+      }
+    >
+      {/* Pulls the text from createYourActivity.title */}
+      <h1>{t("buildActivity.createTitle")}</h1>
+
+      <img
+        title="Sparks effect"
+        alt="Sparks effect"
+        src="page3_effect.svg"
+        loading="lazy"
+        width={23}
+        height={24}
+      />
+    </div>
+  );
 }
 
 export default CreateYourActivity;
