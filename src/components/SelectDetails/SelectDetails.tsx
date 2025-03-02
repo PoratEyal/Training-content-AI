1b9192b1a281da3354459d271e79608ac3fe379c
PoratEyal <eyal1.porat@gmail.com>
2025-03-02 18:16:07 +0200

    more changes in the languege

--- src/components/SelectDetails/SelectDetails.tsx
+++ src/components/SelectDetails/SelectDetails.tsx
@@ -1,44 +1,51 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SelectOption } from "../../models/types/common";
 import styles from "./SelectDetails.module.css";
+import { useTranslation } from "react-i18next";
 
 type SelectDetailsProps = {
-    data: SelectOption[];
-    placeholder: string;
-    obj: string;
-    setObj: (obj: string) => void;
+  data: SelectOption[];
+  placeholder: string;
+  obj: string;
+  setObj: (obj: string) => void;
 };
 
 function SelectDetails({ data, placeholder, obj, setObj }: SelectDetailsProps) {
-    const [selected, setIsSelected] = useState(obj ? true : false);
+  const [selected, setIsSelected] = useState(!!obj);
+  const { i18n } = useTranslation();
+  const isHebrew = i18n.language === "he"; // or i18n.dir() === "rtl"
 
-    useEffect(() => {
-        setIsSelected(obj ? true : false);
-    }, [obj]);
+  useEffect(() => {
+    setIsSelected(!!obj);
+  }, [obj]);
 
-    const handleChnage = (value: string) => {
-        setObj(value);
-        setIsSelected(value !== "" || obj ? true : false);
-    };
+  const handleChange = (value: string) => {
+    setObj(value);
+    setIsSelected(value !== "" || !!obj);
+  };
 
-    return (
-        <div className={styles.input_div}>
-            <select
-                style={{ color: selected ? "#333335" : "#8b8b8b" }}
-                value={obj}
-                onChange={(e) => handleChnage(e.target.value)}
-                aria-label={placeholder}
-            >
-                <option value="" disabled>
-                    {placeholder}
-                </option>
-                {data.map((option) => (
-                    <option key={option.value} value={option.value}>
-                        {option.label}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+  return (
+    <div
+      // Conditionally apply .ltr_div if not Hebrew
+      className={!isHebrew ? `${styles.input_div} ${styles.ltr_div}` : styles.input_div}
+    >
+      <select
+        style={{ color: selected ? "#333335" : "#8b8b8b" }}
+        value={obj}
+        onChange={(e) => handleChange(e.target.value)}
+        aria-label={placeholder}
+      >
+        <option value="" disabled>
+          {placeholder}
+        </option>
+        {data.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
+
 export default SelectDetails;
