1427a95c3cddd34934ecc7d7dd740457afde6464
PoratEyal <eyal1.porat@gmail.com>
2025-03-02 18:16:07 +0200

    more changes in the languege

--- src/components/SubjectInput/SubjectInput.tsx
+++ src/components/SubjectInput/SubjectInput.tsx
@@ -5,49 +5,66 @@ import MagicBtn from "../MagicBtn/MagicBtn";
 import magic from "../../models/resources/magic.json";
 import { CategoryName } from "../../models/types/movement";
 
+// Import i18n to detect language
+import { useTranslation } from "react-i18next";
+
 type SubjectInputProps = {
-    placeholder?: string;
-    subject: string;
-    category?: CategoryName;
-    setSubject: React.Dispatch<React.SetStateAction<string>>;
-    setHasAlert: React.Dispatch<React.SetStateAction<boolean>>;
+  placeholder?: string;
+  subject: string;
+  category?: CategoryName;
+  setSubject: React.Dispatch<React.SetStateAction<string>>;
+  setHasAlert: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 function SubjectInput({
-    placeholder,
-    subject,
-    category,
-    setSubject,
-    setHasAlert,
+  placeholder,
+  subject,
+  category,
+  setSubject,
+  setHasAlert,
 }: SubjectInputProps) {
-    const handleInputChange = (event) => {
-        const newValue = event.target.value as string;
-        // Limit the value to 30 characters
-        if (newValue.length <= 30) {
-            const isBlackListed = isInBlackList(newValue);
-            setHasAlert(isBlackListed);
-            setSubject(newValue);
-        }
-    };
+  const { i18n } = useTranslation();
+  const isHebrew = i18n.language === "he"; // or i18n.dir() === "rtl"
 
-    const changeSubject = (newSubject: string) => {
-        setSubject(newSubject);
-    };
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newValue = event.target.value;
+    // Limit the value to 30 characters
+    if (newValue.length <= 30) {
+      const isBlackListed = isInBlackList(newValue);
+      setHasAlert(isBlackListed);
+      setSubject(newValue);
+    }
+  };
 
-    return (
-        <div className={styles.input_and_icon}>
-            <textarea
-                content="width=device-width, initial-scale=1.0, user-scalable=no"
-                className={styles.input}
-                value={subject}
-                onChange={handleInputChange}
-                placeholder={placeholder || ""}
-            />
-            {magic[category] ? (
-                <MagicBtn options={magic[category]} setSubject={changeSubject} />
-            ) : null}
+  const changeSubject = (newSubject: string) => {
+    setSubject(newSubject);
+  };
+
+  return (
+    <div
+      className={
+        // If you want the entire container to flip direction:
+        isHebrew 
+          ? styles.input_and_icon 
+          : `${styles.input_and_icon} ${styles.ltr}`
+      }
+    >
+      <textarea
+        content="width=device-width, initial-scale=1.0, user-scalable=no"
+        className={styles.input}
+        value={subject}
+        onChange={handleInputChange}
+        placeholder={placeholder || ""}
+      />
+
+      {/* Wrap MagicBtn in an absolute-positioned div so we can shift sides */}
+      {magic[category] && (
+        <div className={isHebrew ? styles.magic_btn_rtl : styles.magic_btn_ltr}>
+          <MagicBtn options={magic[category]} setSubject={changeSubject} />
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default SubjectInput;
