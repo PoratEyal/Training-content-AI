efeea05e811c296beb56190ccc6e0d297d0a0a04
PoratEyal <eyal1.porat@gmail.com>
2025-03-09 20:18:29 +0200

    working on the dropdwon

--- src/components/options/dropdown/DropOptInviteFriends.tsx
+++ src/components/options/dropdown/DropOptInviteFriends.tsx
@@ -1,20 +1,24 @@
+import React from "react";
 import styles from "./dropdown.module.css";
 import { WhatsappShareButton } from "react-share";
 import { formatInviteFriend } from "../../../utils/format";
 import { WEBSITE_URL } from "../../../models/constants";
 import { Icons } from "../../Icons";
+import { useTranslation } from "react-i18next";
 
 function DropOptInviteFriends() {
-    return (
-        <WhatsappShareButton
-            className={styles.text_and_icon}
-            url={WEBSITE_URL}
-            title={formatInviteFriend()}
-        >
-            שתפו עם חברים 
-            <Icons.share />
-        </WhatsappShareButton>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <WhatsappShareButton
+      className={styles.text_and_icon}
+      url={WEBSITE_URL}
+      title={formatInviteFriend()}
+    >
+      {t("profile.dropOptInviteFriends.invite")}
+      <Icons.share />
+    </WhatsappShareButton>
+  );
 }
 
 export default DropOptInviteFriends;
