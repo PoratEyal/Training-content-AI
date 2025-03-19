f765a55eaf7a709b34710dd88eff8b88019f19d3
royporat <roy.porat@bloxroute.com>
2025-03-19 09:30:29 +0200

    cleanup context

--- src/components/Layout/UnderBar/UnderBar.tsx
+++ src/components/Layout/UnderBar/UnderBar.tsx
@@ -1,9 +1,9 @@
 import styles from "./UnderBar.module.css";
-import NavOptBuild from "../options/navbar/NavOptBuild";
-import NavOptContent from "../options/navbar/NavOptContent";
-import NavOptHome from "../options/navbar/NavOptHome";
-import NavOptMyActivities from "../options/navbar/NavOptMyActivities";
-import { useAuthContext } from "../../context/AuthContext";
+import NavOptBuild from "../../options/navbar/NavOptBuild";
+import NavOptContent from "../../options/navbar/NavOptContent";
+import NavOptHome from "../../options/navbar/NavOptHome";
+import NavOptMyActivities from "../../options/navbar/NavOptMyActivities";
+import { useAuthContext } from "../../../context/AuthContext";
 
 const UnderBar = () => {
     const { isLoggedIn } = useAuthContext();
