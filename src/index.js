96f4ecf60dd7d8ce3d45dc95d1ce13d578e25fe9
shayashertlv <shay.j.asher@gmail.com>
2025-05-13 19:10:55 +0300

    eyal -  added logic for languges by location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import './i18n/i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { detectCountry } from "./utils/geo";
+import { initI18n } from "./i18n/i18n";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const bootstrap = async () => {
+  const country = await detectCountry();
+  const defaultLng = country === "IL" ? "he" : "en";
+  await initI18n(defaultLng);
+
+  const rootEl = document.getElementById("root");
+  if (!rootEl) return;
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
       <App />
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+};
+
+bootstrap();
