4d8caf9fa68ef11d5274b43640e631eb56ec12dd
LiorPdev <lior.porat@gmail.com>
2025-05-24 12:30:18 +0300

    Update movment.ts

--- src/models/resources/movment.ts
+++ src/models/resources/movment.ts
@@ -244,7 +244,7 @@ export const Movements = {
             categories: [
                 {
                     name: "contant",
-                    title: "Contant Activity",
+                    title: "Content Activity",
                 },
                 {
                     name: "playTime",
