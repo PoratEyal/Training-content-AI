b04e440f37499a0d0ffbf1bdee756f6aa6bbad0d
Lior Porat <lior@example.com>
2025-05-27 17:20:19 +0300

    lior - fixed the english

--- src/components/LangPopup/LangPopup.tsx
+++ src/components/LangPopup/LangPopup.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import * as Flags from "country-flag-icons/react/3x2";
 import styles from "./LangPopup.module.css";
 import { useLanguage } from "../../i18n/useLanguage";
+import { useAuthContext } from "../../context/AuthContext";
+import { fetchUpdateUser } from "../../utils/fetch";
+import { useContentContext } from "../../context/ContentContext";
+import { useNavigate } from "react-router-dom";
+import route from "../../router/route.json";
 
 type LangPopupProps = {
     handleClose: () => void;
@@ -9,10 +14,31 @@ type LangPopupProps = {
 
 const LangPopup: React.FC<LangPopupProps> = ({ handleClose }) => {
     const { changeLang } = useLanguage();
+    const { currentUser } = useAuthContext();
+    const { clearAll } = useContentContext();
+    const navigate = useNavigate();
 
-    const changeLanguage = (lang: string) => {
+    const changeLanguage = async (lang: string) => {
+        sessionStorage.removeItem("data");
+
+        if (currentUser) {
+            await fetchUpdateUser({
+                user: {
+                    ...currentUser,
+                    movement: {
+                        movement: null,
+                        grade: null,
+                        gender: null,
+                        amount: null,
+                    },
+                },
+            });
+        }
+
+        clearAll();
         changeLang(lang);
         handleClose();
+        navigate(route.home);
     };
 
     return (
