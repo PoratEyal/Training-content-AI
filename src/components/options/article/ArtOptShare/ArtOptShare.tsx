761fb16035060e3b28495f0ea59ba37564504c26
shayashertlv <shay.j.asher@gmail.com>
2025-05-29 19:02:58 +0300

    Eyal - fix the share logic

--- src/components/options/article/ArtOptShare/ArtOptShare.tsx
+++ src/components/options/article/ArtOptShare/ArtOptShare.tsx
@@ -1,27 +1,46 @@
 import React from "react";
 import styles from "./ArtOptShare.module.css";
 import { FaShare } from "react-icons/fa";
-import { WhatsappShareButton } from "react-share";
 import { Activity } from "../../../../models/types/activity";
-import { formatWhatsUp } from "../../../../utils/format";
 import { WEBSITE_URL } from "../../../../models/constants";
+import { formatWhatsUp } from "../../../../utils/format";
 import { useLanguage } from "../../../../i18n/useLanguage";
 
-type ArtOptShareProps = {
-    activity: Activity;
-};
+type ArtOptShareProps = { activity: Activity };
 
 const ArtOptShare: React.FC<ArtOptShareProps> = ({ activity }) => {
-    const { t, dir } = useLanguage();
-    return (
-        <WhatsappShareButton
-            className={`${styles.shareBtn} ${styles[dir]}`}
-            url={WEBSITE_URL}
-            title={formatWhatsUp(activity?.activity)}
-        >
-            <FaShare className={styles.icon} />
-            <span className={styles.text}>{t('articleOptions.share.title')}</span>
-        </WhatsappShareButton>
+  const { t, dir } = useLanguage();
+
+  const handleShare = async () => {
+    const shareText = `${formatWhatsUp(activity?.activity)}\n\n${WEBSITE_URL}`;
+
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: t("articleOptions.share.title"),
+          text: shareText, 
+        });
+        return;
+      } catch {
+      }
+    }
+
+    window.open(
+      `https://wa.me/?text=${encodeURIComponent(shareText)}`,
+      "_blank"
     );
+  };
+
+  return (
+    <button
+      className={`${styles.shareBtn} ${styles[dir]}`}
+      onClick={handleShare}
+      type="button"
+    >
+      <FaShare className={styles.icon} />
+      <span className={styles.text}>{t("articleOptions.share.title")}</span>
+    </button>
+  );
 };
+
 export default ArtOptShare;
