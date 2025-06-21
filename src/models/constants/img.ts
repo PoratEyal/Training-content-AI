17efd7708cac5baaf56df53d68f05568b8d6a799
Lior Porat <lior@example.com>
2025-06-21 12:41:28 +0300

    add another banner

--- src/models/constants/img.ts
+++ src/models/constants/img.ts
@@ -1,9 +1,3 @@
 //https://imagekit.io/
-export const AI_IMG = "https://ik.imagekit.io/df9g0ij1q/ai.svg?updatedAt=1716100702200";
-export const BIG_LOGO_IMG = "https://ik.imagekit.io/df9g0ij1q/bigLogo.svg?updatedAt=1716100702465";
-export const H2_PAGE2_IMG = "https://ik.imagekit.io/df9g0ij1q/h2_page2.svg?updatedAt=1716100702129";
-export const HAND_ICON_IMG = "https://ik.imagekit.io/df9g0ij1q/hand_icon.svg?updatedAt=1716100702052";
 export const LAMP_IMG = "https://ik.imagekit.io/df9g0ij1q/lamp.svg?updatedAt=1716100702095";
-export const MORE_IMG = "https://ik.imagekit.io/df9g0ij1q/more.svg?updatedAt=1716100702195";
-export const PAGE3_EFFECT_IMG = "https://ik.imagekit.io/df9g0ij1q/page3_effect.svg?updatedAt=1716100702186";
 export const PATH_IMG = "https://ik.imagekit.io/df9g0ij1q/path.svg?updatedAt=1716100702125";
