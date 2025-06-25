e981afa15dad027180f126f799f073ccedd9adc8
Lior Porat <lior@example.com>
2025-06-25 10:05:01 +0300

    fix share dbl URL

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -5,17 +5,15 @@ export const useShareTextOrLink = () => {
   const { handleSuccess, handleError } = useErrorContext()
 
   return (t: TFunction, title: string, text: string, url?: string) => {
-    const fullText = url ? `${text}\n\n${url}` : text
-
     if (navigator.share) {
-console.log("title: ",title, "text: ", fullText, "url: ", url)
-      navigator.share({ title, text: fullText, url })
+      navigator.share({ title, text, url })
         .catch((err) => {
           if (err.name !== "AbortError" && err.name !== "NotAllowedError") {
             handleError(t("share.shareError"))
           }
         })
     } else {
+      const fullText = url ? `${text}\n\n${url}` : text
       navigator.clipboard
         .writeText(fullText)
         .then(() => {
